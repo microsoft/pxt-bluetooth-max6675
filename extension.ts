92ce39d304b3c9752a42d332e54fdcb96a7a3cf3
Peli de Halleux <jhalleux@microsoft.com>
2017-03-31 02:31:30 -0700

    support for specifying pin

--- extension.ts
+++ extension.ts
@@ -1,10 +1,14 @@
 namespace bluetooth {
+    /**
+     * Starts a Bluetooth temperature service listening
+     * to a MAX6675 on the given pin. Should only be called once
+     */
     //% block
-    export function startMax6675Service() {
+    export function startMax6675Service(pin: DigitalPin) {
         if (bluetooth.internalStartMax6675Service()) {
             control.inBackground(() => {
                 while (true) {
-                    bluetooth.max6675SetTemperature(input.max6675Temperature(DigitalPin.P0));
+                    bluetooth.max6675SetTemperature(input.max6675Temperature(pin));
                     basic.pause(bluetooth.max6675Period())
                 }
             })
