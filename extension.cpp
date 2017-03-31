5e825707e7100f66ae8005a036836e9b41d9d86a
Peli de Halleux <jhalleux@microsoft.com>
2017-03-31 01:48:53 -0700

    initial commit

---
+++ extension.cpp
@@ -0,0 +1,8 @@
+#include "pxt.h"
+using namespace pxt;
+namespace bluetooth_max6675 {
+    //%
+    int extfun(int x, int y) {
+        return x + y;
+    }
+}
---
+++ extension.ts
@@ -0,0 +1,10 @@
+namespace bluetooth_max6675 {
+    /**
+     * Help goes here.
+     */
+    //% shim=bluetooth_max6675::extfun
+    export function extfun(x: number, y: number) {
+        // Dummy implementation for the simulator.
+        return x - y
+    }
+}
