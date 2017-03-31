c627b12022b6571b0d33634c5d0e416bb6d111f2
Peli de Halleux <jhalleux@microsoft.com>
2017-03-31 09:14:07 -0700

    update with sensor ref

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 A Bluetooth temperature service reading from a MAX6675 device
 
+## Usage
+
+Simply call ``startMax6675Service`` with the pin where the MAX6675 is connected
+to start a service that beams the temperature every second.
+
+```blocks
+bluetooth.startMax6675Service(DigitalPin.P0);
+```
+
 ## License
 
 MIT
@@ -11,3 +20,8 @@ MIT
 * for PXT/microbit
 (The metadata above is needed for package search.)
 
+```package
+bluetooth
+bluetooth-temperature-sensor
+bluetooth-max6675
+```
--- extension.ts
+++ extension.ts
@@ -1,17 +1,12 @@
 namespace bluetooth {
     /**
      * Starts a Bluetooth temperature service listening
-     * to a MAX6675 on the given pin. Should only be called once
+     * to a MAX6675 on the given pin. Should only be called once.
      */
     //% block
     export function startMax6675Service(pin: DigitalPin) {
-        if (bluetooth.internalStartMax6675Service()) {
-            control.inBackground(() => {
-                while (true) {
-                    bluetooth.max6675SetTemperature(input.max6675Temperature(pin));
-                    basic.pause(bluetooth.max6675Period())
-                }
-            })
-        }    
+        bluetooth.startTemperatureSensor(1000, () => {
+            bluetooth.setTemperatureSensorValue(input.max6675Temperature(pin));
+        })
     }
 }
